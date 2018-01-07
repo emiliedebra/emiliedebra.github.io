407037191744e443b00b87174eb2f9e5b482ff5e
Emilie Wood <emilie@anotherway.co.za>
2018-01-07 13:38:32 +0200

    update index routing

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('index.html', fs.readFileSync('index.html', 'utf8').replace(/src=\//g, 'src='));
+fs.writeFileSync('index.html', fs.readFileSync('index.html', 'utf8').replace(/\/static\//g, 'dist/static/'));
+// fs.writeFileSync('index.html', fs.readFileSync('index.html', 'utf8').replace(/src=\//g, 'src='));
